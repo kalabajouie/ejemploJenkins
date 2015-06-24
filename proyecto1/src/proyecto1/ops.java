43292c98b5d53ff9599834f7d4af38ba584abce8
Luis <Luis@192.168.1.7>
2015-06-23 23:23:37 -0600

    todo el proyecto

---
+++ proyecto1/src/proyecto1/ops.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyecto1;
+
+/**
+ *
+ * @author Luis
+ */
+public class ops {
+    
+    int suma(int n1, int n2){
+
+        int res = n1+n2;
+        return res;
+
+}
+
+
+int resta(int n1, int n2){
+
+    int res = n1-n2;
+    return res;
+}
+    
+}
